d3d2a9b8f8cf6fad3a6334cab954c163d84dc310
Emmanuel Frécon <efrecon@gmail.com>
2024-03-23 17:07:14 +0100

    Fix prompt

--- demo/demo.sh
+++ demo/demo.sh
@@ -4,7 +4,8 @@ DEMO_ROOTDIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 
 . "$DEMO_ROOTDIR/demo-magic/demo-magic.sh"
 
-DEMO_PROMPT="${GREEN}➜ ${CYAN}\W ${COLOR_RESET}"
+#DEMO_PROMPT="${GREEN}➜ ${CYAN}\W ${COLOR_RESET}"
+DEMO_PROMPT="${GREEN}➜ ${COLOR_RESET}"
 
 if [ -z "${RUNNER_PAT:-}" ]; then
   echo "Create a RUNNER_PAT environment variable with a GitHub Personal Access Token"
@@ -15,7 +16,8 @@ clear
 
 pei "# Let's start one (artificially short-lived) runner for this repository"
 pei "# A PAT is present in the environment variable RUNNER_PAT"
-pei "# Just for the demo, we will use two seldom used options to show teardown"
-pei "# -k 40: to run for 40 seconds only, the default is to run forever, until a job is picked"
-pei "# -r 1: to run once only, the default is to create ephemeral runners forever"
+pei "# Just for the demo, we will use two seldom used options to show teardown:"
+pei "#   -k 40: to run for 40 seconds only, the default is to run forever, until a job is picked"
+pei "#   -r 1: to run once only, the default is to create ephemeral runners forever"
 pei "./orchestrator.sh -v -- -r 1 -k 40 -p efrecon/gh-runner-krunvm"
+sleep 5
